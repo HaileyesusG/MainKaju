a33026e43d9e9ff5715f5eeb6249abb126231b07
HaileyesusG <hailegetnet2020@gmail.com>
2024-10-07 20:32:33 +0300

    hundredtwentyfive commit

--- frontend/src/Components/withDraw.jsx
+++ frontend/src/Components/withDraw.jsx
@@ -3,7 +3,11 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 import { io } from "socket.io-client";
 const socket = io("https://mainkaju.onrender.com");
 const WithdrawalForm = ({ user }) => {
-  console.log("the token is ", user);
+  let token = user ? "Bearer " + user.token : "";
+  if (token == "Bearer " + undefined) {
+    token = user ? "Bearer " + user.token : "";
+  }
+  console.log("the token is ", token);
   const [formData, setFormData] = useState({
     fullname: "",
     email: "",
