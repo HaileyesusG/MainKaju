2d1754383c254aeca66cf481f2b0e9b79f34e646
HaileyesusG <hailegetnet2020@gmail.com>
2024-10-07 21:58:06 +0300

    hundredtwentyfour commit

--- frontend/src/Components/Profile.jsx
+++ frontend/src/Components/Profile.jsx
@@ -121,7 +121,7 @@ const UserProfile = ({ user4 }) => {
   const handleSignOut = () => {
     localStorage.removeItem("user");
     dispatch2(logOut([]));
-    red("/");
+    red("/login");
   };
   const handleLicense = (e) => {
     setLicense(e.target.value);
