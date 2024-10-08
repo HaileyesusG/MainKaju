8504361088376fc3d7d9ccf70149429f1c9e0c99
HaileyesusG <hailegetnet2020@gmail.com>
2024-10-08 19:38:38 +0300

    hfourthy1 commit

--- frontend/src/Components/AdminChat.jsx
+++ frontend/src/Components/AdminChat.jsx
@@ -5,6 +5,7 @@ import { useSendMessage } from "../Hooks/useSendMessage";
 import { IoSend } from "react-icons/io5";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { FaRegCircle } from "react-icons/fa";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 import { setChat, removeChat } from "../features/chat/chatSlice";
 const AdminChat = ({ user3 }) => {
